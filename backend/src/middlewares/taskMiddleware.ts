2ecdaffbbe95429a894bf6c08fed13cc2fb50063
Alef Sloan <Alef_sloan@hotmail.com>
2022-05-18 22:03:21 -0300

    refactor: solving eslint problems

--- backend/src/middlewares/taskMiddleware.ts
+++ backend/src/middlewares/taskMiddleware.ts
@@ -5,9 +5,9 @@ export default class TaskMiddleware {
   public static validateTitle: RequestHandler = async (req, res, next) => {
     const { title } = req.body;
 
-    if(!title) return res.status(400).json({ message: "Fields \"Title\" must be filled" });
+    if(!title) return res.status(400).json({ message: 'Fields "Title" must be filled' });
 
-    if(title.length <= 5) return res.status(400).json({ message: "Fields \"Title\" must be at least 5 characters long" });
+    if(title.length <= 5) return res.status(400).json({ message: 'Fields "Title" must be at least 5 characters long' });
     
     next();
   }
@@ -15,10 +15,10 @@ export default class TaskMiddleware {
   public static validateStatus: RequestHandler = async (req, res, next) => {
     const { status } = req.body;
 
-    if(!status) return res.status(400).json({ message: "Fields \"Status\" must be filled" });
+    if(!status) return res.status(400).json({ message: 'Fields "Status" must be filled' });
 
     if(status !== 'pendente' && status !== 'em andamento' && status !== 'pronto') {
-      return res.status(400).json({ message: "Fields \"Status\" must be \"pendente\", \"em andamento\" or \"pronto\"" });
+      return res.status(400).json({ message: 'Fields "Status" must be "pendente", "em andamento" or "pronto"' });
     }
 
     next();
