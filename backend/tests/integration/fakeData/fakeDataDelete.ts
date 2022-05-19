e458660e0752491ba1524355d9529aa50b762917
Alef Sloan <Alef_sloan@hotmail.com>
2022-05-19 11:55:20 -0300

    test: add test to delete route methods

---
+++ backend/tests/integration/fakeData/fakeDataDelete.ts
@@ -0,0 +1,12 @@
+const fakeDeleteTaskData = {
+  mock: {
+		id: 1,
+		createdAt: "2022-05-18T21:52:00.757Z",
+		title: "Arrumar o quarto",
+		status: "pronto"
+	},
+  response: {  },
+  request: {  }
+}
+
+export default fakeDeleteTaskData;
