726aaade06fda7df8e020ceb885f87509bbddc2b
Alef Sloan <Alef_sloan@hotmail.com>
2022-05-19 12:00:56 -0300

    test: add test to patch route methods

---
+++ backend/tests/integration/fakeData/fakeDataPatch.ts
@@ -0,0 +1,17 @@
+const fakePatchData = {
+  mock: {
+		id: 1,
+		createdAt: "2022-05-18T21:52:00.757Z",
+		title: "Arrumar o quarto",
+		status: "pronto"
+	},
+  response: {
+		id: 1,
+		createdAt: "2022-05-18T21:52:00.757Z",
+		title: "Arrumar o quarto",
+		status: "pronto"
+	},
+  request: { }
+}
+
+export default fakePatchData;
